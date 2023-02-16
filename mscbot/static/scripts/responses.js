66a605340509697bb4911d1d0508a3b38eac3de9
Sachin Goyal <114551556+Sachingoyal46@users.noreply.github.com>
2023-02-16 19:32:50 +0530

    Add files via upload

---
+++ mscbot/static/scripts/responses.js
@@ -0,0 +1,189 @@
+function getBotResponse(input) {
+    //symptoms of disesies
+    if (input == "symptoms of diabeties") {
+        return "Symptoms may include frequent urination, excessive thirst or hunger, and weight changes.";
+    } else if (input == "symptoms of blood pressure") {
+        return " nervousness, sweating, difficulty sleeping or facial flushing";
+    } else if (input == "symptoms of heartattack") {
+        return " Pain in the neck, back, shoulders or jaw Shortness of breath Abdominal pain or heartburn";
+    }
+    else if(input=="symptoms of periods")
+    {
+        return " abdominal cramps, headache, tender breasts, and changes in bowel movements."
+    }
+    else if(input=="symptoms of cancer")
+    {
+        return " unusual diarrhea or constipation. Difficulty in swallowing or continuing indigestion."
+    }
+    else if(input=="symptoms of dengue")
+    {
+        return " Belly pain, tendernes"
+        
+    }
+    else if(input=="symptoms of malariya")
+    {
+        return " discomfort. Headache. Nausea and vomiting. Diarrhea."
+    }
+    
+
+    // medicines
+//****************************************************************************************** */
+    else if(input=="abilify")
+    {
+        return "Abilify is an antipsychotic medication. It works by changing the actions of chemicals in the brain.";
+    }
+    else if(input=="acetylcysteine")
+    {
+        return "Acetylcysteine is used to help prevent or lessen liver damage caused by taking large quantities of acetaminophen (Tylenol)."
+        
+    }
+    else if(input=="actos")
+    {
+        return "Actos is an oral diabetes medicine that helps control blood sugar levels."
+    }
+    else if(input=="advil")
+    {
+        return "Advil is a nonsteroidal anti-inflammatory drug (NSAID). Ibuprofen works by reducing hormones that cause inflammation and pain in the body"
+    }
+    else if(input=="aleve")
+    {
+        return "Aleve is a nonsteroidal anti-inflammatory drug (NSAID). Naproxen works by reducing hormones that cause inflammation and pain in the body";
+    }
+    else if(input=="ambien")
+    {
+        return "Ambien is a sedative, also called a hypnotic. Zolpidem affects chemicals in the brain that may be unbalanced in people with sleep problems insomnia).";
+    }
+    else if(input=="ampiciline")
+    {
+        return "Ampicillin is a penicillin antibiotic that is used to treat or prevent many different types of infections such as bladder infections, pneumonia, gonorrhea, meningitis, or infections of the stomach or intestines."
+    }
+    else if(input=="androgel")
+    {
+        return "AndroGel contains testosterone, a naturally occurring male hormone necessary for many processes in the body. It works by replacing or supplementing the testosterone that is naturally made in the body.";
+
+    }
+    else if(input=="aspirin")
+    {
+        return "Aspirin is used to treat pain, and reduce fever or inflammation. It is sometimes used to treat or prevent heart attacks, strokes, and chest pain (angina)"
+    }
+    else if(input=="ascorbic acid")
+    {
+        return "Ascorbic acid (vitamin C) occurs naturally in foods such as citrus fruit, tomatoes, potatoes, and leafy vegetables. Vitamin C is important for bones and connective tissues, muscles, and blood vessels. Vitamin C also helps the body absorb iron, which is needed for red blood cell production.";
+        
+    }
+    else if(input=="atropine")
+    {
+        return "Ophthalmic atropine, homatropine, and scopolamine are used to dilate (enlarge) the pupil of the eye. They are used before eye examinations, before and after eye surgery, and to treat certain eye conditions, such as uveitis or posterior synechiae."
+    }
+    else if(input=="annovera")
+    {
+        return "Annovera Vaginal Rings are made of silicone and contains two female hormones that are slowly released into your vagina and then enter your blood. The vaginal ring contains a combination of segesterone acetate and ethinyl estradiol."
+    }
+    else if(input=="adderall")
+    {
+        return "Adderall is used to treat attention deficit hyperactivity disorder (ADHD) and narcolepsy. Adderall contains a combination of amphetamine and dextroamphetamine. Amphetamine and dextroamphetamine are central nervous system stimulants that affect chemicals in the brain and nerves that contribute to hyperactivity and impulse control."
+    }
+    else if(input=="albuterol")
+    {
+        return "Albuterol is a bronchodilator that relaxes muscles in the airways and increases air flow to the lungs."
+    }
+    else if(input=="advair")
+    {
+        return "Advair contains a combination of fluticasone and salmeterol. Fluticasone is a steroid that prevents the release of substances in the body that cause inflammation. Salmeterol is a bronchodilator. It works by relaxing muscles in the airways to improve breathing"
+    }
+    else if(input=="azathioprine")
+    {
+        return "Azathioprine weakens your body's immune system, to help keep it from  a transplanted organ such as a kidney. Organ rejection happens when the immune system treats the new organ as an invader and attacks it."
+    }
+    //********************************************************************************************************************************************* */
+    else if(input=="bacitracin")
+    {
+        return "For the treatment of superficial ocular infections involving the conjunctiva and/or cornea caused by Bacitracin susceptible organisms.";
+    }
+    else if(input=="baclofen")
+    {
+        return "Baclofen is used to treat muscle pain, spasms, and stiffness in people with multiple sclerosis or spinal cord injury or disease.";
+    }
+    else if(input=="bactrim")
+    {
+        return "Bactrim contains a combination of sulfamethoxazole and trimethoprim. Sulfamethoxazole and trimethoprim are are both antibiotics that treat different types of infection caused by bacteria.";
+    }
+    else if(input=="banzel")
+    {
+        return "Banzel is used together with other medicines to treat seizures caused by Lennox-Gastaut syndrome, a severe form of childhood epilepsy that also causes developmental and behavior problems";
+    }
+    else if(input=="benicar")
+    {
+        return "Benicar is an angiotensin II receptor blocker (sometimes called an ARB). Olmesartan keeps blood vessels from narrowing, which lowers blood pressure and improves blood flow.";
+    }
+    else if(input=="bentyl")
+    {
+        return "Bentyl is used to treat functional bowel or irritable bowel syndrome.";
+    }
+    else if(input=="bethanechol")
+    {
+        return "Bethanechol is used to treat urinary retention (trouble urinating) that may be caused by surgery, delivering a baby, or other conditions.";
+    }
+    else if(input=="biaxin")
+    {
+        return "The Biaxin brand name has been discontinued in the U.S. If generic versions of this product have been approved by the FDA, there may be generic equivalents available.";
+    }
+    else if(input=="biotin")
+    {
+        return "Biotin has been used in alternative medicine as a likely effective aid in treating or preventing biotin deficiency. Biotin deficiency can be caused by malnutrition, rapid weight loss, long-term tube feeding, and other medical conditions.";
+    }
+    else if(input=="boniva")
+    {
+        return "Boniva is a bisphosphonate medicine that alters bone formation and breakdown in the body. This can slow bone loss and may help prevent bone fractures.";
+    }
+    else if(input=="botox")
+    {
+        return "Botox injection is a prescription medicine that helps improve the appearance of facial wrinkles, and treats some medical conditions, by causing muscles to relax, as botox blocks nerve signals to the muscles";
+    }
+    else if(input=="bumex")
+    {
+        return "Bumetanide belongs to a group of medicines called loop diuretics or  Bumetanide is given to help treat fluid retention (edema) and swelling that is caused by congestive heart failure, liver disease, kidney disease, or other medical conditions. It works by acting on the kidneys to increase the flow of urine";
+    }
+    else if(input=="buprenex")
+    {
+        return "Buprenex Injection is used to relieve moderate pain that is severe enough to require an opiate pain killer and for which other medicines (e.g., non-opiate pain killers or opiate-containing combination medicines) have not been, or are not expected to be sufficient.";
+    }
+    else if(input=="buspar")
+    {
+        return "BuSpar is used to treat anxiety disorders or the symptoms of anxiety, such as fear, tension, irritability, dizziness, pounding heartbeat, and other physical symptoms.";
+    }
+    else if(input=="bystolic")
+    {
+        return "Bystolic belongs to a group of drugs called beta-blockers. Beta-blockers affect the heart and circulation (blood flow through arteries and veins).";
+    }
+    else if(input=="benlysta")
+    {
+        return "Benlysta is a monoclonal antibody that affects the actions of the body's immune system. Monoclonal antibodies are made to target and destroy only certain cells in the body. This may help to protect healthy cells from damage.";
+    }
+   
+
+
+    //******************************************************************************************************************************************************* */
+
+
+    // Simple responses
+    else if (input == "hii") {
+        return "Hello there!,how can i help you";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } 
+    else if(input=="thankyou")
+    {
+        return "Welcome Dear!";
+    }
+    else if(input="give me the information of medicine")
+    {
+        return "sure! enter the name of medicine!";
+    }
+    
+    else {
+        return "Try asking something else!";
+    }
+
+    
+}
